a5867540d815566f6ad82b4de53d2c79142f0e7e
Alita Alice <3441425824@qq.com>
2020-09-04 19:35:51 +0800

    [mod]设计模式

--- src/main/java/com/alita/designpattern/factoryInstance/impl/WhiteHuman.java
+++ src/main/java/com/alita/designpattern/factoryInstance/impl/WhiteHuman.java
@@ -15,12 +15,10 @@ public class WhiteHuman implements Human {
     @Override
     public void getColor() {
         System.out.println("白色皮肤");
-
     }
 
     @Override
     public void talk() {
         System.out.println("ENGLISH");
-
     }
 }
