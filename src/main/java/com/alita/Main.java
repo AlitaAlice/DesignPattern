7f13a78efb857655a16604398e4a42db302dc09f
AlitaAlice <3441425824@qq.com>
2020-09-08 20:16:52 +0800

    s

--- src/main/java/com/alita/Main.java
+++ src/main/java/com/alita/Main.java
@@ -1,59 +1,61 @@
 package com.alita;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    boolean[][] isVisited;
 
-    public boolean exist(char[][] board, String word) {
-        return helper(board, word.toCharArray());
-    }
-
-    private boolean helper(char[][] board, char[] words) {
-        isVisited = new boolean[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == words[0]) {
-                    if (tryFind(board, words, 1, j, i)) return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    //当前的wordIndex,nowX,nowY是否可以得到这个单词
-    private boolean tryFind(char[][] board, char[] words, int wordIndex, int nowX, int nowY) {
-        if (isVisited[nowY][nowX])
-            return false;
-        if (wordIndex == words.length)
-            return true;
-        isVisited[nowY][nowX] = true;
-        boolean success = false;
-        if (wordIndex < words.length) {
-            boolean b1, b2, b3, b4;
-            b1 = b2 = b3 = b4 = false;
-            if (nowX - 1 >= 0 && board[nowY][nowX - 1] == words[wordIndex])
-                b1 = tryFind(board, words, wordIndex + 1, nowX - 1, nowY);
-            if (nowX + 1 < board[0].length && board[nowY][nowX + 1] == words[wordIndex])
-                b2 = tryFind(board, words, wordIndex + 1, nowX + 1, nowY);
-            if (nowY - 1 >= 0 && board[nowY - 1][nowX] == words[wordIndex])
-                b3 = tryFind(board, words, wordIndex + 1, nowX, nowY - 1);
-            if (nowY + 1 < board.length && board[nowY + 1][nowX] == words[wordIndex])
-                b4 = tryFind(board, words, wordIndex + 1, nowX, nowY + 1);
-            success = b1 || b2 || b3 || b4;
-        }
-        if (!success)
-            isVisited[nowY][nowX] = false;
-        return success;
-    }
-    public static void main(String[] args) {
+//    public static String replacestr(String target,String source )
+//    {
+//        char[] chars = target.toCharArray();
+//        /**
+//         * a b c d
+//         */
+//
+//        int length = chars.length * chars.length;
+//        String[] strings = new String[length];
+//        for (int i = 0; i <length ; i++) {
+//            strings[i] = String.valueOf(chars[0]);
+//        }
+//
+//
+//    }
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        char[][] chars = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
-        Main main = new Main();
-        System.out.println(main.exist(chars, str));
-
+       String str = scanner.nextLine();
+       String Source = scanner.nextLine();
+       String  repalce = scanner.nextLine();
+        List<String> result = new ArrayList<>();
+        getAllList(result, str.toCharArray(), 0);
+        String target =Source;
+        for(String each : result){
+             target = target.replaceAll(each, repalce);
+        }
+         System.out.println(target);
     }
+    public static void getAllList(List<String> result, char[] strs, int begin){
+        if(begin == strs.length-1){
+            result.add(String.valueOf(strs));
+        }
+        for(int i = begin; i < strs.length; i++){
+            swap(strs, i, begin);
+            getAllList(result, strs, begin+1);
+            swap(strs, i, begin);
+        }
+    }
+
+    private static void swap(char[] strs, int index1, int index2) {
+        char temp = strs[index1];
+        strs[index1] = strs[index2];
+        strs[index2] = temp;
+    }
+
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        String str = "\u00234\1234";
+//        System.out.println(str.length());
+//    }
 
 }
 
